7c7298f93370244bb419914243d540300a888018
Daniel Ashton <danielashton@nadadventist.org>
2016-07-25 18:11:37 -0400

    Initial commit, empty test file

---
+++ MoneyTest.elm
@@ -0,0 +1,16 @@
+module MoneyTest  exposing (..)
+
+import ElmTest exposing (..)
+
+
+tests : Test
+tests =
+    suite "Suite of Money tests"
+        [
+        ]
+
+
+
+main : Program Never
+main =
+  runSuiteHtml tests
