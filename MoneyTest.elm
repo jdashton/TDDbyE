4cc14fd67217f9e892a46bf4b851368d29439e2c
Daniel Ashton <danielashton@nadadventist.org>
2016-07-25 19:22:30 -0400

    Finished ch 1

--- MoneyTest.elm
+++ MoneyTest.elm
@@ -1,14 +1,16 @@
 module MoneyTest  exposing (..)
 
 import ElmTest exposing (..)
+import Dollar exposing (..)
+
 
 
 tests : Test
 tests =
     suite "Suite of Money tests"
-        [
+        [ test "Multiplication"
+            <| assertEqual 10 (Dollar.init 5 |> times 2 |> amount)
         ]
-
 
 
 main : Program Never
