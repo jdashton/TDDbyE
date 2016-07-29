282604390988888ef904afa3364777b28b4c4ee1
Daniel Ashton <danielashton@nadadventist.org>
2016-07-29 12:08:23 -0400

    Finish chapter 2

--- MoneyTest.elm
+++ MoneyTest.elm
@@ -7,9 +7,15 @@ import Dollar exposing (..)
 
 tests : Test
 tests =
+  let
+      five =
+        Dollar.init 5
+  in
     suite "Suite of Money tests"
         [ test "Multiplication"
-            <| assertEqual 10 (Dollar.init 5 |> times 2 |> amount)
+            <| assertEqual 10 (times 2 five |> amount)
+        , test "Mult2"
+            <| assertEqual 15 (times 3 five |> amount)
         ]
 
 
