fc17de18fe6391cfdd708e0e524da11802d982f9
Daniel Ashton <danielashton@nadadventist.org>
2016-07-29 06:23:19 -0400

    Finish chapter 4

--- MoneyTest.java
+++ MoneyTest.java
@@ -5,8 +5,13 @@ public class MoneyTest extends TestCase {
   // Test methods go here
 
   public void testMultiplication() {
-    Dollar five= new Dollar(5);
-    five.times(2);
-    assertEquals(10, five.amount);
+    Dollar five = new Dollar(5);
+    assertEquals(new Dollar(10), five.times(2));
+    assertEquals(new Dollar(15), five.times(3));
+  }
+
+  public void testEquality() {
+    assertTrue(new Dollar(5).equals(new Dollar(5)));
+    assertFalse(new Dollar(5).equals(new Dollar(6)));
   }
 }
