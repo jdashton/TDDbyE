d1d3c18d40a210842be08e3ba0bb0d77f444cee8
Daniel Ashton <danielashton@nadadventist.org>
2016-08-09 15:18:18 -0400

    Finish ch 5 - elm

--- MoneyTest.elm
+++ MoneyTest.elm
@@ -2,6 +2,7 @@ module MoneyTest  exposing (..)
 
 import ElmTest exposing (..)
 import Dollar exposing (..)
+import Franc exposing (..)
 
 
 
@@ -12,10 +13,12 @@ tests =
         Dollar 5
   in
     suite "Suite of Money tests"
-        [ test "Multiplication10" <| assertEqual (Dollar 10) (five |> times 2)
-        , test "Multiplication15" <| assertEqual (Dollar 15) (five |> times 3)
-        , test "Equality"         <| assert <| Dollar 5 == Dollar 5
-        , test "Inequality"       <| assert <| Dollar 5 /= Dollar 6
+        [ test "Multiplication10"       <| assertEqual (Dollar 10) (five |> Dollar.times 2)
+        , test "Multiplication15"       <| assertEqual (Dollar 15) (five |> Dollar.times 3)
+        , test "Multiplication10 Franc" <| assertEqual (Franc 10) (five |> Franc.times 2)
+        , test "Multiplication15 Franc" <| assertEqual (Franc 15) (five |> Franc.times 3)
+        , test "Equality"               <| assert <| Dollar 5 == Dollar 5
+        , test "Inequality"             <| assert <| Dollar 5 /= Dollar 6
         ]
 
 
