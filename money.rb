dfafd380853fb5e724a2fce3dc6a5e3724e19293
Daniel Ashton <danielashton@nadadventist.org>
2016-08-09 10:55:28 -0400

    Finish ch 7 - ruby

--- money.rb
+++ money.rb
@@ -1,11 +1,17 @@
 class Money
 
+  public
+
   def == (money)
-    @amount == money.amount
+    @amount == money.amount && self.class == money.class
   end
 
   protected
 
   attr_reader :amount
+
+  private
+
+  @amount = 0
 
 end
