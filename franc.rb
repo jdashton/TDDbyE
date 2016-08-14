23681feb5aa2f7886dff9ad340811b1486b723c4
Daniel Ashton <danielashton@nadadventist.org>
2016-08-14 19:13:12 -0400

    Finish ch 9 - ruby

--- franc.rb
+++ franc.rb
@@ -1,10 +1,5 @@
 class Franc < Money
 
-  def initialize (amount, currency)
-    @amount = amount
-    @currency = 'CHF'
-  end
-
   def times (multiplier)
     Money.franc @amount * multiplier
   end
--- money.rb
+++ money.rb
@@ -2,27 +2,27 @@ class Money
 
   attr_reader :currency
 
-  private
-
-  @amount = 0
-  @currency = ''
-
-  public
+  def initialize (amount, currency)
+    @amount = amount
+    @currency = currency
+  end
 
   def == (money)
     @amount == money.amount && self.class == money.class
   end
 
   def self.dollar(amount)
-    Dollar.new amount
+    Dollar.new amount, 'USD'
   end
 
   def self.franc(amount)
-    Franc.new amount, nil
+    Franc.new amount, 'CHF'
   end
 
   protected
 
   attr_reader :amount
+  @amount = 0
+  @currency = ''
 
 end
