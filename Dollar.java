d09eec46cada19ae8a4209772e85193624e5d3a3
Daniel Ashton <danielashton@nadadventist.org>
2016-08-14 18:10:10 -0400

    p40-java

--- Dollar.java
+++ Dollar.java
@@ -1,10 +1,17 @@
 class Dollar extends Money {
 
+  private String currency;
+
   Dollar(int amount) {
     this.amount = amount;
+    currency = "USD";
   }
 
   Money times(int multiplier) {
     return new Dollar(amount * multiplier);
+  }
+
+  String currency() {
+    return currency;
   }
 }
--- Franc.java
+++ Franc.java
@@ -1,10 +1,17 @@
 class Franc extends Money {
 
+  private String currency;
+
   Franc(int amount) {
     this.amount = amount;
+    currency = "CHF";
   }
 
   Money times(int multiplier) {
     return new Franc(amount * multiplier);
+  }
+
+  String currency() {
+    return currency;
   }
 }
