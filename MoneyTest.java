fe0a5e866f42b9a46a3e2cc0ace0196c39019977
Daniel Ashton <danielashton@nadadventist.org>
2016-08-21 16:40:06 -0400

    p56-java

--- MoneyTest.java
+++ MoneyTest.java
@@ -25,4 +25,8 @@ public class MoneyTest extends TestCase {
     Money sum = Money.dollar(5).plus(Money.dollar(5));
     assertEquals(Money.dollar(10), sum);
   }
+
+  public void testSimpleAddition() {
+    assertEquals(Money.dollar(10), reduced);
+  }
 }
