8571b22137b1ed8bff355ef933c3f6a284dfe8e8
Daniel Ashton <danielashton@nadadventist.org>
2016-08-21 17:08:06 -0400

    Finish ch 12 - java

--- MoneyTest.java
+++ MoneyTest.java
@@ -22,11 +22,7 @@ public class MoneyTest extends TestCase {
   }
 
   public void testSimpleAddition() {
-    Money sum = Money.dollar(5).plus(Money.dollar(5));
-    assertEquals(Money.dollar(10), sum);
-  }
-
-  public void testSimpleAddition() {
+    Money five = Money.dollar(5);
     Expression sum = five.plus(five);
     Bank bank = new Bank();
     Money reduced = bank.reduce(sum, "USD");
