a84b92fdff38d8765fa07b00c59e5ba7d11feb9d
Daniel Ashton <danielashton@nadadventist.org>
2016-08-21 18:27:40 -0400

    Finish ch 11 - ruby

--- money_test.rb
+++ money_test.rb
@@ -1,7 +1,6 @@
 require 'minitest/autorun'
 require 'minitest/reporters'
 require_relative 'money'
-require_relative 'franc'
 
 Minitest::Reporters.use!
 
@@ -10,12 +9,6 @@ class MoneyTest < Minitest::Test
     five = Money.dollar 5
     assert_equal Money.dollar(10), five.times(2)
     assert_equal Money.dollar(15), five.times(3)
-  end
-
-  def test_franc_multiplication
-    five = Money.franc 5
-    assert_equal Money.franc(10), five.times(2)
-    assert_equal Money.franc(15), five.times(3)
   end
 
   def test_equality
@@ -27,9 +20,5 @@ class MoneyTest < Minitest::Test
   def test_currency
     assert_equal 'USD', Money.dollar(1).currency
     assert_equal 'CHF', Money.franc(1).currency
-  end
-
-  def test_different_class_equality
-    assert Money.new(10, 'CHF') == Franc.new(10, 'CHF')
   end
 end
