d0c6473b50d7125a77018b15a3406829ce0e04a8
Daniel Ashton <danielashton@nadadventist.org>
2016-08-21 16:24:32 -0400

    Finish ch 11 - java

--- MoneyTest.java
+++ MoneyTest.java
@@ -16,18 +16,8 @@ public class MoneyTest extends TestCase {
     assertFalse(Money.franc(5).equals(Money.dollar(5)));
   }
 
-  public void testFrancMultiplication() {
-    Money five = Money.franc(5);
-    assertEquals(Money.franc(10), five.times(2));
-    assertEquals(Money.franc(15), five.times(3));
-  }
-
   public void testCurrency() {
     assertEquals("USD", Money.dollar(1).currency());
     assertEquals("CHF", Money.franc(1).currency());
-  }
-
-  public void testDifferentClassEquality() {
-    assertTrue(new Money(10, "CHF").equals(new Franc(10, "CHF")));
   }
 }
