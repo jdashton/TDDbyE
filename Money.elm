ab7bdf5d4625f5b74af07ab61675f399999ed637
Daniel Ashton <danielashton@nadadventist.org>
2016-08-22 12:05:59 -0400

    p45-elm

--- Money.elm
+++ Money.elm
@@ -2,25 +2,32 @@ module Money exposing (..)
 
 
 type Money
-  = Dollar Int
-  | Franc Int
+  = Dollar Int String
+  | Franc Int String
+
+
+dollar : Int -> Money
+dollar amount =
+  Dollar amount "USD"
+
+
+franc : Int -> Money
+franc amount =
+  Franc amount "CHF"
 
 
 times : Int -> Money -> Money
 times multiplier money =
   case money of
-    Dollar amount ->
-      Dollar <| amount * multiplier
+    Dollar amount currency ->
+      dollar <| amount * multiplier
 
-    Franc amount ->
-      Franc <| amount * multiplier
+    Franc amount currency ->
+      franc <| amount * multiplier
 
 
 currency : Money -> String
 currency money =
   case money of
-    Dollar _ ->
-      "USD"
-
-    Franc _ ->
-      "CHF"
+    Dollar _ currency -> currency
+    Franc  _ currency -> currency
