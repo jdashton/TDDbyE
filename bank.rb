c8635c08f4cc3a0a39354fc88f83a080fcc50206
Daniel Ashton <danielashton@nadadventist.org>
2016-08-26 17:20:07 -0400

    Finish ch 13 - ruby

--- bank.rb
+++ bank.rb
@@ -1,7 +1,6 @@
 
 class Bank
   def reduce(source, to)
-    return source.reduce(to) if source.instance_of?(Money)
     source.reduce to
   end
 end
--- expression.rb
+++ expression.rb
@@ -1,2 +1,6 @@
 class Expression
+  def reduce (to)
+    raise NotImplementedError.
+      new "#{self.class.name}#area is an abstract method."
+  end
 end
