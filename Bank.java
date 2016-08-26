f52ab75207431ff19fa3c57c496148bd8c7a1eed
Daniel Ashton <danielashton@nadadventist.org>
2016-08-26 16:23:52 -0400

    p69-java

--- Bank.java
+++ Bank.java
@@ -1,4 +1,8 @@
+import java.util.Hashtable;
+
 class Bank {
+  private Hashtable rates = new Hashtable();
+
   int rate(String from, String to) {
     return (from.equals("CHF") && to.equals("USD"))
       ? 2
@@ -7,5 +11,24 @@ class Bank {
 
   Money reduce(Expression source, String to) {
     return source.reduce(this, to);
+  }
+
+  private class Pair {
+    private String from;
+    private String to;
+
+    Pair(String from, String to) {
+      this.from = from;
+      this.to   = to;
+    }
+
+    public boolean equals(Object object) {
+      Pair pair = (Pair) object;
+      return from.equals(pair.from) && to.equals(pair.to);
+    }
+
+    public int hashCode() {
+      return 0;
+    }
   }
 }
