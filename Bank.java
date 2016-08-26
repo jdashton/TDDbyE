6c4c15f9e551eb7e8795c2af6f2629ef9bd62241
Daniel Ashton <danielashton@nadadventist.org>
2016-08-26 16:14:12 -0400

    p68-java

--- Bank.java
+++ Bank.java
@@ -1,5 +1,11 @@
 class Bank {
+  int rate(String from, String to) {
+    return (from.equals("CHF") && to.equals("USD"))
+      ? 2
+      : 1;
+  }
+
   Money reduce(Expression source, String to) {
-    return source.reduce(to);
+    return source.reduce(this, to);
   }
 }
