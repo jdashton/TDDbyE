6848c724a2ef4598207620412829e0684523e65e
Daniel Ashton <danielashton@nadadventist.org>
2016-09-01 16:49:21 -0400

    Finish ch 16 - ruby

--- expression.rb
+++ expression.rb
@@ -1,11 +1,16 @@
 class Expression
   def reduce(bank, to)
     raise NotImplementedError.
-      new "#{self.class.name}#area is an abstract method."
+      new "#{self.class.name}#reduce is an abstract method."
   end
 
   def plus(addend)
     raise NotImplementedError.
-      new "#{self.class.name}#area is an abstract method."
+      new "#{self.class.name}#plus is an abstract method."
+  end
+
+  def times(multiplier)
+    raise NotImplementedError.
+      new "#{self.class.name}#times is an abstract method."
   end
 end
