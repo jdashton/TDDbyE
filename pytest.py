d7fa3d78ddfdfc4aadaa20bbadb5f7f9d72c892b
Daniel Ashton <danielashton@nadadventist.org>
2016-09-10 21:16:58 -0400

    Finish ch 20 - python

--- pytest.py
+++ pytest.py
@@ -4,29 +4,35 @@ class TestCase:
         self.name = name
 
     def run(self):
+        # result.testStarted()
         self.setUp()
         method = getattr(self, self.name)
         method()
+        self.tearDown()
 
     def setUp(self):
+        pass
+
+    def tearDown(self):
         pass
 
 class WasRun(TestCase):
     def __init__(self, name):
         TestCase.__init__(self, name)
 
+    def setUp(self):
+        self.log = "setUp "
+
     def testMethod(self):
-        self.wasRun = 1
         self.log = self.log + "testMethod "
 
-    def setUp(self):
-        self.wasRun = None
-        self.log = "setUp "
+    def tearDown(self):
+        self.log = self.log + "tearDown "
 
 class TestCaseTest(TestCase):
     def testTemplateMethod(self):
         test = WasRun("testMethod")
         test.run()
-        assert("setUp testMethod " == test.log)
+        assert("setUp testMethod tearDown " == test.log)
 
 TestCaseTest("testTemplateMethod").run()
