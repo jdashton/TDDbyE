c02d33f2c1b01ad14fe2a68d6358632c3830c053
Daniel Ashton <danielashton@nadadventist.org>
2016-09-13 15:59:41 -0400

    p109-python

--- pytest.py
+++ pytest.py
@@ -21,9 +21,13 @@ class TestCase:
 class TestResult:
     def __init__(self):
         self.runCount = 0
+        self.errorCount = 0
 
     def testStarted(self):
         self.runCount = self.runCount + 1
+
+    def testFailed(self):
+        self.errorCount = self.errorCount + 1
 
     def summary(self):
         return "%d run, 0 failed" % self.runCount
@@ -58,7 +62,16 @@ class TestCaseTest(TestCase):
     def testFailedResult(self):
         test = WasRun("testBrokenMethod")
         result = test.run()
-        assert("1 run, 1 failed" == result.summary)
+        assert("1 run, 1 failed" == result.summary())
 
-TestCaseTest("testTemplateMethod").run()
-TestCaseTest("testResult").run()
+    def testFailedResultFormatting(self):
+        result = TestResult()
+        result.testStarted()
+        result.testFailed()
+        assert("1 run, 1 failed" == result.summary())
+
+
+# TestCaseTest("testTemplateMethod").run()
+# TestCaseTest("testResult").run()
+# TestCaseTest("testFailedResult").run()
+TestCaseTest("testFailedResultFormatting").run()
