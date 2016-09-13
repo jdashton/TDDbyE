a604f771a14326eb15d62169cdc7af56b30f8ed2
Daniel Ashton <danielashton@nadadventist.org>
2016-09-13 15:41:20 -0400

    Finish ch 21 - python

--- pytest.py
+++ pytest.py
@@ -4,17 +4,29 @@ class TestCase:
         self.name = name
 
     def run(self):
-        # result.testStarted()
+        result = TestResult()
+        result.testStarted()
         self.setUp()
         method = getattr(self, self.name)
         method()
         self.tearDown()
+        return result
 
     def setUp(self):
         pass
 
     def tearDown(self):
         pass
+
+class TestResult:
+    def __init__(self):
+        self.runCount = 0
+
+    def testStarted(self):
+        self.runCount = self.runCount + 1
+
+    def summary(self):
+        return "%d run, 0 failed" % self.runCount
 
 class WasRun(TestCase):
     def __init__(self, name):
@@ -29,10 +41,24 @@ class WasRun(TestCase):
     def tearDown(self):
         self.log = self.log + "tearDown "
 
+    def testBrokenMethod(self):
+        raise Exception
+
 class TestCaseTest(TestCase):
     def testTemplateMethod(self):
         test = WasRun("testMethod")
         test.run()
         assert("setUp testMethod tearDown " == test.log)
 
+    def testResult(self):
+        test = WasRun("testMethod")
+        result = test.run()
+        assert("1 run, 0 failed" == result.summary())
+
+    def testFailedResult(self):
+        test = WasRun("testBrokenMethod")
+        result = test.run()
+        assert("1 run, 1 failed" == result.summary)
+
 TestCaseTest("testTemplateMethod").run()
+TestCaseTest("testResult").run()
